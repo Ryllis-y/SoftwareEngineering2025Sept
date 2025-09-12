59738348cbec15e6a57e16c4e6a58c0da44c9e9e
yrh <3390398857@qq.com>
2025-09-12 20:35:35 +0800

    添加脚本目录的内容

---
+++ Assets/RotateCube.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+public class RotateCube : MonoBehaviour{
+    public float speed = 30f;
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+}
